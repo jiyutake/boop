ca14e1cb5717eed1dcb1b8600d7e7258d214f193
Kate <tiffany254@proton.me>
2025-06-23 17:07:20 -0300

    Nevermind. bspwm it is.

--- config/apps.lua
+++
@@ -1,7 +0,0 @@
--- default applications to use.
-local apps = {}
-apps.terminal   = 'wezterm'
-apps.editor     = 'mrcode'
-apps.editor_cmd = apps.terminal .. ' -e ' .. apps.editor
-require('menubar').utils.terminal = apps.terminal
-return apps
